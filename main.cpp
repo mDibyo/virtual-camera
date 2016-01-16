a9263455a05aa3993ad90264b7d81b8c6dab21dc
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-01-15 18:20:07 -0800

    Add basic video viewing capabilities

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <iostream>
+#include "opencv2/opencv.hpp"
 
-using namespace std;
+using namespace cv;
 
-int main() {
-  cout << "Hello, World!" << endl;
+
+int main(int argc, char** argv) {
+  if ( argc != 2 ) {
+    printf("usage: VirtualCamera.video <Video_Path>\n");
+    return -1;
+  }
+
+  VideoCapture capture(argv[1]);
+  namedWindow("Video display window", WINDOW_AUTOSIZE);
+
+  for (;;) {
+    Mat frame;
+    capture >> frame;
+    if (frame.empty()) break;
+    imshow("Video display window", frame);
+    if (waitKey(30) >= 0) break;
+  }
   return 0;
 }
