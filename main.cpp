b53959d4379c4119dddabd614f495d4c8f08dec8
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2016-01-18 16:08:21 -0800

    Add video viewing capabilities

--- main.cpp
+++ main.cpp
@@ -4,20 +4,40 @@ using namespace cv;
 
 
 int main(int argc, char** argv) {
-  if ( argc != 2 ) {
-    printf("usage: VirtualCamera.video <Video_Path>\n");
+  if (argc != 3) {
+    printf("usage: VirtualCamera.inputVideo <Video_Path> VirtualCamera.outputVideo <Video_Path>\n");
     return -1;
   }
 
   VideoCapture capture(argv[1]);
+
+  Size size = Size((int) capture.get(CV_CAP_PROP_FRAME_WIDTH),
+                   (int) capture.get(CV_CAP_PROP_FRAME_HEIGHT));
+  printf("Output file: %s", argv[2]);
+  VideoWriter writer(argv[2], CV_FOURCC('M','J','P','G'), capture.get(CV_CAP_PROP_FPS), size);
+  if (!writer.isOpened()) {
+    printf("Could not open writer. Aborting.");
+    return -1;
+  }
+
   namedWindow("Video display window", WINDOW_AUTOSIZE);
 
   for (;;) {
     Mat frame;
     capture >> frame;
-    if (frame.empty()) break;
+    if (frame.empty()) {
+      break;
+    }
+
+    // Show video and write to new video
     imshow("Video display window", frame);
-    if (waitKey(30) >= 0) break;
+    writer << frame;
+
+    if (waitKey(30) >= 0) {
+      break;
+    }
   }
+  writer.release();
+
   return 0;
 }
